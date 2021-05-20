4846d66c14bce82b7ac04af06a76a7fc13ee3c32
Becca Wyland <rwyland@uwalumni.com>
2021-05-19 23:28:56 -0500

    add variable to store reference to search bar in form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
-var getWeatherConditions = function() {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}").then(function(response) {
+// Reference the form
+const searchByCityEl = document.querySelector("#search-by-city");
+
+// Get current and future weather conditions using One Call API
+const getWeatherConditions = function() {
+    // console.log shows GET request as 401 (unauthorized)
+    // use template literals in this fetch request? lat, lon, part, api key?
+    const apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+    console.log(apiUrl);
+
+    // make a request using fetch
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
