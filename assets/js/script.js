3419b6b2da2749f451b99c7c795bd25dc3fb4dc3
Becca Wyland <rwyland@uwalumni.com>
2021-05-24 15:15:11 -0500

    review code with tutor, learn how to appropriately call apis needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,48 @@
 // Reference the form
-const searchByCityEl = document.querySelector("#search-by-city");
-const searchFormEl = document.querySelector("#search-form");
+const searchByCityEl = document.getElementById("search-by-city");
+const searchFormEl = document.getElementById("search-form");
+
+console.log(searchByCityEl, searchFormEl);
+
+const appId = "&appid=18b93bcb5dc1cfcbc507982a4bf6e1bd";
 
 // Get current and future weather conditions using One Call API
-const getWeatherConditions = function() {
+const getWeatherConditions = function(cityName) {
     // console.log shows GET request as 401 (unauthorized)
     // use template literals in this fetch request? lat, lon, part, api key?
-    const apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}${appId}`;
     console.log(apiUrl);
 
     // make a request using fetch
     fetch(apiUrl).then(function(response) {
+        console.log(response);
         response.json().then(function(data) {
-            //console.log(data);
+            console.log(data);
+
+            const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly${appId}`;
+            console.log(oneCallApiUrl);
+            fetch(oneCallApiUrl).then(function(oneCallData) {
+                oneCallData.json().then(function(data2) {
+                    console.log(data2);
+                });
+            });
+
+        
         });
     });
 };
 
+
+
 // Function to perform upon form submission
 const formSubmitHandler = function(event) {
     event.preventDefault();
+    console.log(searchByCityEl.value);
+    getWeatherConditions(searchByCityEl.value);
     //console.log(event);
 };
 
+searchFormEl.addEventListener("submit", formSubmitHandler);
 
-
-getWeatherConditions();
+//left off on 6.2.4
+// getWeatherConditions();
