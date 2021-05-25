78bdc5578a1f33cb65495770250b2150fd437fbf
Becca Wyland <rwyland@uwalumni.com>
2021-05-24 23:44:46 -0500

    refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,30 @@
 const searchByCityEl = document.getElementById("search-by-city");
 const searchFormEl = document.getElementById("search-form");
 
-console.log(searchByCityEl, searchFormEl);
-
 const appId = "&appid=18b93bcb5dc1cfcbc507982a4bf6e1bd";
 
 // Get current and future weather conditions using One Call API
 const getWeatherConditions = function(cityName) {
-    // console.log shows GET request as 401 (unauthorized)
-    // use template literals in this fetch request? lat, lon, part, api key?
+    // Variable to get city's weather information
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}${appId}`;
-    console.log(apiUrl);
 
     // make a request using fetch
     fetch(apiUrl).then(function(response) {
         console.log(response);
         response.json().then(function(data) {
             console.log(data);
+        });
+    });
 
-            const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly${appId}`;
-            console.log(oneCallApiUrl);
-            fetch(oneCallApiUrl).then(function(oneCallData) {
-                oneCallData.json().then(function(data2) {
-                    console.log(data2);
-                });
-            });
+    
 
-        
+    // Variable to get lat and lon of city
+    const oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly${appId}`;
+    console.log(oneCallApiUrl);
+
+    fetch(oneCallApiUrl).then(function(oneCallData) {
+        oneCallData.json().then(function(data2) {
+            console.log(data2);
         });
     });
 };
